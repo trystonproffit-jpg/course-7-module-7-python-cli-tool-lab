a9e233608be3d9f934f236493305318dcd482bf8
Tryston <trystonproffit@gmail.com>
2026-03-26 14:20:34 -0400

    Finished

--- lib/models.py
+++ lib/models.py
@@ -4,14 +4,12 @@
 
 class Task:
     def __init__(self, title):
-        # TODO: Assign the title
-        # TODO: Set completed to False
-        pass
+        self.title = title
+        self.completed = False
 
     def complete(self):
-        # TODO: Mark the task as complete
-        # TODO: Print a confirmation message
-        pass
+        self.completed = True
+        print(f"✅ Task '{self.title}' completed.")
 
 # TODO: Define the User class
 # Each user has a name and a list of tasks
@@ -19,16 +17,15 @@ class Task:
 
 class User:
     def __init__(self, name):
-        # TODO: Store the user's name
-        # TODO: Initialize an empty list of tasks
-        pass
+        self.name = name
+        self.tasks = []
 
     def add_task(self, task):
-        # TODO: Add the task to the user's task list
-        # TODO: Print a message confirming the task was added
-        pass
+        self.tasks.append(task)
+        print(f"📌 Task '{task.title}' added to {self.name}.")
 
     def get_task_by_title(self, title):
-        # TODO: Search for a task by its title in the user's task list
-        # TODO: Return the matching task or None
-        pass
+        for task in self.tasks:
+            if task.title == title:
+                return task
+        return None
